c05ca24f3bc1d0718a54a6ba72f52f7bde62541d
Daniel Mulvad <daniel@dmulvad.com>
2023-04-19 17:55:24 -0700

    handle CSP with cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -12,15 +12,7 @@ const config = {
 		adapter: adapter({
 			// precompress handled by Cloudflare
 			precompress: false
-		}),
-		csp: {
-			directives: {
-				'script-src': ['self']
-			},
-			reportOnly: {
-				'script-src': ['self']
-			}
-		}
+		})
 	}
 };
 
